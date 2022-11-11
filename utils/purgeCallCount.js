d91398760d55156603b94f7c5bcb596f7e110d77
Jon Pitans <jonpitans@gmail.com>
2022-11-11 17:24:03 +0000

    tidying up

--- utils/purgeCallCount.js
+++ utils/purgeCallCount.js
@@ -2,12 +2,9 @@ const express = require("express");
 const app = express.Router();
 const asyncMySQL = require("../utils/connection");
 
-const { default: axios } = require("axios");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
-const apiKey = process.env.API_KEY;
 
 function purgeCallCount() {
   asyncMySQL(`UPDATE apidata SET monthlycalls = 0;`);
